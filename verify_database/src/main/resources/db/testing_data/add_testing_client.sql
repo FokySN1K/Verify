2761e43ac0ad32db303631c724320e632c7a4133
Yaroslav Stetsenko <yaroslav_stetsenko@inbox.ru>
2026-04-19 21:09:33 +0300

    [verify_database] Добавление тестовых xsd

---
+++ verify_database/src/main/resources/db/testing_data/add_testing_client.sql
@@ -0,0 +1,6 @@
+insert into client (client_id, name, surname, email, role)
+values
+    ('customer_1', 'Иван', 'Заказчиков', 'test1@stetsenko.su', 'CUSTOMER'),
+    ('customer_2', 'Мария', 'Клиентова', 'test2@stetsenko.su', 'CUSTOMER'),
+    ('contractor_1', 'Пётр', 'Подрядчиков', 'test3@stetsenko.su', 'CONTRACTOR'),
+    ('contractor_2', 'Олег', 'Исполнителев', 'test4@stetsenko.su', 'CONTRACTOR');
